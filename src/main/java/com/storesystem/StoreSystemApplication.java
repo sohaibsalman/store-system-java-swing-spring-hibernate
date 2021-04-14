439359eeb2e507345dc5932ea2f75617f87dfb66
Sohaib Salman <sohaibsalman10@gmail.com>
2021-04-14 15:42:43 +0500

    Initial commit

---
+++ src/main/java/com/storesystem/StoreSystemApplication.java
@@ -0,0 +1,14 @@
+package com.storesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreSystemApplication.class, args);
+                System.out.println("Hello World");
+	}
+
+}
